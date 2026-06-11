5a2934797506c3aab7909aee3052a49773cc6002
Nikita Bevziuk <161725878+Bevziuk2005@users.noreply.github.com>
2026-06-11 16:57:35 +0000

    gunicorn update

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,6 +1,12 @@
 import multiprocessing
+import os
 
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 4
-bind = '0.0.0.0:8000'
+
+# Использовать PORT из environment переменной, если есть (Render)
+# Или default 8000 для локальной разработки
+port = os.getenv('PORT', '8000')
+bind = f'0.0.0.0:{port}'
+
 timeout = 120
